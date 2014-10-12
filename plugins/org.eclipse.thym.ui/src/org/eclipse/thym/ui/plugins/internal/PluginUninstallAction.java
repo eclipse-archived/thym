5cc129ab9da15dc1b3a95f481170d6a9795139e2
Gorkem Ercan <gorkem.ercan@gmail.com>
2014-10-11 20:32:24 -0400

    Multiselection when removing cordova plugins via project explorer
    Enhances the PluginUninstallAction to support multiple selected
    CordovaPlugins are removed instead of the first selection only. Changes
    the confirmation dialog to accomodate multiple plugins selected and also
    aligns the dialog and action titles
    
    Signed-off-by: Gorkem Ercan <gorkem.ercan@gmail.com>

--- plugins/org.eclipse.thym.ui/src/org/eclipse/thym/ui/plugins/internal/PluginUninstallAction.java
+++ plugins/org.eclipse.thym.ui/src/org/eclipse/thym/ui/plugins/internal/PluginUninstallAction.java
@@ -11,11 +11,12 @@
 package org.eclipse.thym.ui.plugins.internal;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.ErrorDialog;
@@ -24,6 +25,7 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.thym.core.HybridProject;
 import org.eclipse.thym.core.plugin.CordovaPlugin;
 import org.eclipse.thym.ui.HybridUI;
@@ -32,9 +34,10 @@ import org.eclipse.ui.PlatformUI;
 
 public class PluginUninstallAction extends Action{
 	
+	private static final String CONFIRM_DIALOG_TITLE = "Uninstall Cordova plug-in(s)";
 	private CordovaPlugin plugin;
 	public PluginUninstallAction() {
-		super("Remove Cordova Plug-in");
+		super("Uninstall Cordova plug-in");
 	}
 	
 	public PluginUninstallAction(CordovaPlugin cordovaPlugin) {
@@ -44,32 +47,39 @@ public class PluginUninstallAction extends Action{
 
 	@Override
 	public void run() {
-		if(this.plugin == null ){
+		final ArrayList<CordovaPlugin> pluginsToRemove = new ArrayList<CordovaPlugin>();
+		if(this.plugin != null ){
+			pluginsToRemove.add(plugin);
+		}else{
 			IStructuredSelection selection = getSelection();
 			if(selection.isEmpty())
 				return;
-			Object o = selection.getFirstElement();
-			if(o instanceof CordovaPlugin ){
-				this.plugin = (CordovaPlugin)o;
-			}else{
-				return;
+			@SuppressWarnings("rawtypes")
+			Iterator it = selection.iterator();
+			while(it.hasNext()){
+				Object o = it.next();
+				if(o instanceof CordovaPlugin ){
+					pluginsToRemove.add((CordovaPlugin)o);
+				}
 			}
 		}
-		StringBuilder sb = new StringBuilder();
-		sb.append("Uninstall ");
-		sb.append(plugin.getId());
-		if(plugin.getName() != null && !plugin.getName().isEmpty()){
-			sb.append(" (").append(plugin.getName()).append(")");
-		}
-		sb.append(" plug-in?");
-		
-		if(!MessageDialog.openConfirm(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), 
-				"Uninstall Cordova Plug-in",
-				sb.toString())){
+		if(pluginsToRemove.isEmpty()){
 			return;
 		}
 		
-		final HybridProject project = HybridProject.getHybridProject(plugin.getFolder().getProject());
+		String message =null;
+		if(pluginsToRemove.size() == 1){
+			message = NLS.bind("Are you sure you want to uninstall {0} plug-in?",
+					new String[]{ pluginsToRemove.get(0).getId()});
+		}else{
+			message = NLS.bind("Are you sure you want to uninstall the selected {0} plug-ins?",
+					new String[]{Integer.toString(pluginsToRemove.size())});
+		}
+		
+		if( !MessageDialog.openConfirm(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), CONFIRM_DIALOG_TITLE, message)){
+			return;
+		}
+		
 		try {
 			PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
 
@@ -77,7 +87,11 @@ public class PluginUninstallAction extends Action{
 				public void run(IProgressMonitor monitor) throws InvocationTargetException,
 				InterruptedException {
 					try {
-						project.getPluginManager().unInstallPlugin(plugin.getId(), new NullProgressMonitor());
+						for (CordovaPlugin cordovaPlugin : pluginsToRemove) {
+							HybridProject project = HybridProject.getHybridProject(cordovaPlugin.getFolder().getProject());
+							monitor.subTask(NLS.bind("Uninstalling {0}", cordovaPlugin.getId()));
+							project.getPluginManager().unInstallPlugin(cordovaPlugin.getId(), monitor);
+						}
 					} catch (CoreException e) {
 						throw new InvocationTargetException(e);
 					}
