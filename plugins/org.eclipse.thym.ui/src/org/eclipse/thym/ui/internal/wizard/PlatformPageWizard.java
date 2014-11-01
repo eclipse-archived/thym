ed6b5899b4068cca8991eac6f0a3a2ced735d708
Gorkem Ercan <gorkem.ercan@gmail.com>
2014-11-01 17:52:39 -0400

    enable next/prev button
    Enables the next/previous button by default for PlatformPageWizards
    
    Signed-off-by: Gorkem Ercan <gorkem.ercan@gmail.com>

--- plugins/org.eclipse.thym.ui/src/org/eclipse/thym/ui/internal/wizard/PlatformPageWizard.java
+++ plugins/org.eclipse.thym.ui/src/org/eclipse/thym/ui/internal/wizard/PlatformPageWizard.java
@@ -191,4 +191,9 @@ public abstract class PlatformPageWizard extends Wizard{
         }
         return canFinish;
     }
+    
+    @Override
+    public boolean needsPreviousAndNextButtons() {
+    	return true;
+    }
 }
