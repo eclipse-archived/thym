77f47b080d9c80ce194ef25c18f89adf458f84f7
James Dubee <jwdubee@us.ibm.com>
2016-03-15 15:55:03 -0400

    [489651] Thym does not support cordova-windows
    Remove unused imports, and variables
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=489651
    Signed-off-by: James Dubee <jwdubee@us.ibm.com>

--- plugins/org.eclipse.thym.win.core/src/org/eclipse/thym/win/internal/core/WinLaunchDelegate.java
+++ plugins/org.eclipse.thym.win.core/src/org/eclipse/thym/win/internal/core/WinLaunchDelegate.java
@@ -13,10 +13,6 @@
 
 package org.eclipse.thym.win.internal.core;
 
-import java.io.File;
-import java.text.MessageFormat;
-import java.util.Map;
-
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
@@ -25,24 +21,17 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate2;
 import org.eclipse.osgi.util.NLS;
-import org.eclipse.thym.core.HybridMobileStatus;
 import org.eclipse.thym.core.HybridProject;
 import org.eclipse.thym.core.HybridProjectLaunchConfigConstants;
 import org.eclipse.thym.win.core.WinCore;
 import org.eclipse.thym.win.core.vstudio.WinBuild;
-import org.eclipse.thym.win.core.vstudio.WinConstants;
-
 import org.eclipse.thym.core.internal.cordova.CordovaCLI;
-import org.eclipse.thym.core.internal.cordova.CordovaCLI.Command;
 
 public class WinLaunchDelegate implements ILaunchConfigurationDelegate2 {
-
-	private File buildArtifact;
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
@@ -52,10 +41,7 @@ public class WinLaunchDelegate implements ILaunchConfigurationDelegate2 {
 		IProject kernelProject = getProject(configuration);
 		Assert.isNotNull(kernelProject,
 				Messages.WinLaunchDelegate_NoProjectError);
-		int deviceId = configuration.getAttribute(
-				WinConstants.ATTR_DEVICE_IDENTIFIER, -1);
 		
-
 		HybridProject project = HybridProject.getHybridProject(kernelProject);
 		if (project == null) {
 			throw new CoreException(new Status(IStatus.ERROR, WinCore.PLUGIN_ID,
@@ -83,7 +69,6 @@ public class WinLaunchDelegate implements ILaunchConfigurationDelegate2 {
 		WinBuild build = new WinBuild();
 		build.init(getProject(configuration), null);
 		build.buildNow(monitor);
-		buildArtifact = build.getBuildArtifact();
 		return false;
 	}
 
